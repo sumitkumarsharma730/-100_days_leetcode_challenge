3d10a685571bc20ed6252888b662c04084168e62
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-02 11:00:13 +0530

    Time: 0 ms (100%), Space: 18.2 MB (59.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& image,
+                                  int sr, int sc, int color) {
+
+        if (image[sr][sc] == color)
+            return image;
+
         int m = image.size();
         int n = image[0].size();
-        queue<pair<int, int>> q;
+
+        int original = image[sr][sc];
+
+        queue<pair<int,int>> q;
         q.push({sr, sc});
-        int val = image[sr][sc];
         image[sr][sc] = color;
-        while(!q.empty()){
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
             auto [x, y] = q.front();
             q.pop();
-            if(x + 1 < m && image[x+1][y] == val){
-                q.push({x+1, y});
-                image[x+1] [y] = color;
-            }
-            if(x > 0 && image[x-1][y] == val){
-                q.push({x-1, y});
-                image[x-1][y]  = color;
-            }
-            if(y + 1 < n && image[x][y+1] == val){
-                q.push({x, y+1});
-                image[x][y+1] = color;
-            }
-            if(y > 0 && image[x][y-1] == val){
-                q.push({x, y-1});
-                image[x][y-1] = color;
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx >= 0 && nx < m &&
+                    ny >= 0 && ny < n &&
+                    image[nx][ny] == original) {
+
+                    image[nx][ny] = color;
+                    q.push({nx, ny});
+                }
             }
         }
+
         return image;
     }
 };
