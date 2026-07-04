1ec74fea6f61c247ff772b5a315068acf51daad9
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-04 20:24:56 +0530

    Time: 0 ms (100%), Space: 14.7 MB (39.12%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,55 +4,63 @@ public:
         int m = board.size();
         int n = board[0].size();
 
-        int x[] = {1, 0, -1, 0};
-        int y[] = {0, 1, 0, -1};
+        queue<pair<int, int>> q;
 
-        vector<vector<int>> visited(m, vector<int>(n, 0));
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
 
+        // First and last column
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                board[i][0] = '#';
+                q.push({i, 0});
+            }
+
+            if (board[i][n - 1] == 'O') {
+                board[i][n - 1] = '#';
+                q.push({i, n - 1});
+            }
+        }
+
+        // First and last row
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                board[0][j] = '#';
+                q.push({0, j});
+            }
+
+            if (board[m - 1][j] == 'O') {
+                board[m - 1][j] = '#';
+                q.push({m - 1, j});
+            }
+        }
+
+        // BFS from border O
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx >= 0 && nx < m &&
+                    ny >= 0 && ny < n &&
+                    board[nx][ny] == 'O') {
+
+                    board[nx][ny] = '#';
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        // Final conversion
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
-                if (board[i][j] == 'O' && !visited[i][j]) {
-                    queue<pair<int, int>> q;
-                    vector<pair<int, int>> marked;
-
-                    bool surr = true;
-
-                    q.push({i, j});
-                    visited[i][j] = 1;
-
-                    while (!q.empty()) {
-                        auto [a, b] = q.front();
-                        q.pop();
-
-                        marked.push_back({a, b});
-
-                        if (a == 0 || a == m - 1 ||
-                            b == 0 || b == n - 1) {
-                            surr = false;
-                        }
-
-                        for (int k = 0; k < 4; k++) {
-                            int nx = a + x[k];
-                            int ny = b + y[k];
-
-                            if (nx >= 0 && nx < m &&
-                                ny >= 0 && ny < n &&
-                                board[nx][ny] == 'O' &&
-                                !visited[nx][ny]) {
-
-                                visited[nx][ny] = 1;
-                                q.push({nx, ny});
-                            }
-                        }
-                    }
-
-                    if (surr) {
-                        for (auto [a, b] : marked) {
-                            board[a][b] = 'X';
-                        }
-                    }
-                }
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
             }
         }
     }
