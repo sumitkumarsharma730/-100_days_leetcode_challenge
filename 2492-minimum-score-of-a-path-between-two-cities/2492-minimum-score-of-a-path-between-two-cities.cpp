28ab9e97675fe75394508c19228793441e34e880
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-04 18:54:47 +0530

    Time: 148 ms (11.4%), Space: 154.6 MB (7.34%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,12 +1,18 @@
 class Solution {
 private: 
-    void dfs(unordered_map<int, list<pair<int, int>>>& adj, vector<int>& visited, queue<int>& q, int& ans){
-        while(!q.empty()){
+    void bfs(unordered_map<int, list<pair<int, int>>>& adj,
+             vector<int>& visited,
+             queue<int>& q,
+             int& ans) {
+
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
-            for(auto it : adj[node]){
+
+            for (auto it : adj[node]) {
                 ans = min(ans, it.second);
-                if(!visited[it.first]){
+
+                if (!visited[it.first]) {
                     visited[it.first] = 1;
                     q.push(it.first);
                 }
@@ -17,16 +23,26 @@ private:
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         unordered_map<int, list<pair<int, int>>> adj;
-        vector<int> visited(n+1, 0);
-        for(int i = 0; i < roads.size(); i++){
-            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
-            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        vector<int> visited(n + 1, 0);
+
+        for (int i = 0; i < roads.size(); i++) {
+            adj[roads[i][0]].push_back(
+                {roads[i][1], roads[i][2]}
+            );
+
+            adj[roads[i][1]].push_back(
+                {roads[i][0], roads[i][2]}
+            );
         }
+
         queue<int> q;
         q.push(1);
         visited[1] = 1;
+
         int ans = INT_MAX;
-        dfs(adj, visited, q, ans);
+
+        bfs(adj, visited, q, ans);
+
         return ans;
     }
 };
