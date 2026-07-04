314b3abc3fc557104529a78a73d063eb897f7969
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-04 16:56:59 +0530

    Time: 28 ms (49.05%), Space: 16.8 MB (57.5%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,24 @@
 class Solution {
 private:
-    void bfs(vector<vector<char>>& grid, int i, int j) {
+    void dfs(int i, int j,
+             vector<vector<char>>& grid,
+             vector<vector<bool>>& vis) {
+
         int m = grid.size();
         int n = grid[0].size();
 
-        queue<pair<int, int>> q;
+        if (i < 0 || i >= m ||
+            j < 0 || j >= n ||
+            grid[i][j] == '0' ||
+            vis[i][j])
+            return;
 
-        q.push({i, j});
-        grid[i][j] = '0';
+        vis[i][j] = true;
 
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-
-        while (!q.empty()) {
-            auto node = q.front();
-            q.pop();
-
-            int x = node.first;
-            int y = node.second;
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                if (nx >= 0 && nx < m &&
-                    ny >= 0 && ny < n &&
-                    grid[nx][ny] == '1') {
-
-                    grid[nx][ny] = '0';
-                    q.push({nx, ny});
-                }
-            }
-        }
+        dfs(i + 1, j, grid, vis);
+        dfs(i - 1, j, grid, vis);
+        dfs(i, j + 1, grid, vis);
+        dfs(i, j - 1, grid, vis);
     }
 
 public:
@@ -39,18 +26,20 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        int island = 0;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+
+        int islands = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                if (grid[i][j] == '1') {
-                    island++;
-                    bfs(grid, i, j);
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    islands++;
+                    dfs(i, j, grid, vis);
                 }
             }
         }
 
-        return island;
+        return islands;
     }
 };
