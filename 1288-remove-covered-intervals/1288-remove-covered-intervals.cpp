237999c23799607de0eb46cf94cbaacb50672136
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-07 12:03:29 +0530

    Time: 3 ms (42.47%), Space: 15.2 MB (30.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,36 +1,24 @@
-// Time: O(n²) | Space: O(n)
+// Time O(n log n), space O(1)
+// sorting is only possible because 0 <= li < ri <= 105 is given intervals[i] = [li, ri];
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        vector<int> covered(n, 0);
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int>& a, vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];
 
-        for (int i = 0; i < n; i++) {
-            if (covered[i]) continue;
-
-            int a = intervals[i][0];
-            int b = intervals[i][1];
-
-            for (int j = i + 1; j < n; j++) {
-                if (covered[j]) continue;
-
-                int c = intervals[j][0];
-                int d = intervals[j][1];
-
-                if (c <= a && b <= d) {
-                    covered[i] = 1;
-                    break;
-                }
-                else if (a <= c && d <= b) {
-                    covered[j] = 1;
-                }
-            }
-        }
+                 return a[0] < b[0];
+             });
 
         int count = 0;
+        int maxEnd = 0;
 
-        for (int x : covered) {
-            if (!x) count++;
+        for (auto& interval : intervals) {
+            if (interval[1] > maxEnd) {
+                count++;
+                maxEnd = interval[1];
+            }
         }
 
         return count;
