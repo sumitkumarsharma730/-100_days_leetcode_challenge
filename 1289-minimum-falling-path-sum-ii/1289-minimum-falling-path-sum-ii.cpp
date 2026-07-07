45b861564edfe1f818a12f3c03f79a45d8f1e2d1
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-07 15:13:27 +0530

    Time: 0 ms (100%), Space: 19.9 MB (92.07%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -3,47 +3,38 @@ public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        int prevCol = -1;
-        int currCol = -1;
-
-        int answer = 0;
-        int prevChangeCost = INT_MAX;
+        int minValue = 0;
+        int secondMin = 0;
+        int minCol = -1;
 
         for (int row = 0; row < n; row++) {
-
-            int minValue = INT_MAX;
-            int secondMin = INT_MAX;
-            int actualMin = INT_MAX;
+            int newMin = INT_MAX;
+            int newSecondMin = INT_MAX;
+            int newMinCol = -1;
 
             for (int col = 0; col < n; col++) {
+                int sum = grid[row][col];
 
-                actualMin = min(actualMin, grid[row][col]);
+                if (col == minCol)
+                    sum += secondMin;
+                else
+                    sum += minValue;
 
-                if (col == prevCol)
-                    continue;
-
-                if (grid[row][col] < minValue) {
-                    secondMin = minValue;
-                    minValue = grid[row][col];
-                    currCol = col;
+                if (sum < newMin) {
+                    newSecondMin = newMin;
+                    newMin = sum;
+                    newMinCol = col;
                 }
-                else if (grid[row][col] < secondMin) {
-                    secondMin = grid[row][col];
+                else if (sum < newSecondMin) {
+                    newSecondMin = sum;
                 }
             }
 
-            answer += minValue;
-
-            int currentLoss = minValue - actualMin;
-
-            if (currentLoss > prevChangeCost) {
-                answer += prevChangeCost - currentLoss;
-            }
-
-            prevChangeCost = secondMin - minValue;
-            prevCol = currCol;
+            minValue = newMin;
+            secondMin = newSecondMin;
+            minCol = newMinCol;
         }
 
-        return answer;
+        return minValue;
     }
 };
