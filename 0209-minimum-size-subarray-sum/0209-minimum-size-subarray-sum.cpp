154c8528b96ae6142b1b75ea5b4758dbb0d2037c
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-08 19:16:55 +0530

    Time: 0 ms (100%), Space: 42 MB (15.91%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        if(n == 0){
-            return 0;
-        }
-        vector<int> PrefixSum(n+1, 0);
-        for(int i = 0; i < n; i++){
-            PrefixSum[i+1] = PrefixSum[i] + nums[i];
-        }
-        vector<int> minlensub(n+1, n+1);
-        for(int i = 1; i <= n; i++){
-            if(PrefixSum[i] >= target){
-                int minlength = i;
-                int j = 1;
-                while(j <= n && PrefixSum[i] - PrefixSum[j] >= target){
-                    minlength = min(minlength, i-j);
-                    j++;
-                }
-                minlensub[i] = minlength;
+        int left = 0;
+        int sum = 0;
+        int minLength = n + 1;
+
+        for (int right = 0; right < n; right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                minLength = min(minLength, right - left + 1);
+
+                sum -= nums[left];
+                left++;
             }
         }
-        int minlength = n+1;
-        for(int i = 0; i <= n; i++){
-            minlength = min(minlength, minlensub[i]);
-        }
-        return (minlength == n+1) ? 0 : minlength;
+
+        return (minLength == n + 1) ? 0 : minLength;
     }
 };
