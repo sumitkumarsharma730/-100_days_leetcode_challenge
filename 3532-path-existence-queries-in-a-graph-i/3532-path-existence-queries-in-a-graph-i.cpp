24a1a5c926cd728c7b53850bf5f377a2c0c7eb67
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-09 11:42:57 +0530

    Time: 19 ms (58.36%), Space: 224.3 MB (59.45%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<bool> ans(queries.size(), 0);
+        vector<bool> adjedge(n, 1);
+        vector<int> preEdge(n, 1);
+        for(int i = 0; i < n-1; i++){
+            if(nums[i+1] - nums[i] > maxDiff){
+                adjedge[i+1] = 0;
+            }
+            preEdge[i+1] = preEdge[i] + adjedge[i+1];
+        }
         for(int a = 0; a < queries.size(); a++){
             int i = queries[a][0];
             int j = queries[a][1];
@@ -9,19 +17,22 @@ public:
                 ans[a] = 1;
             }
             else{
-                bool edge = 1;
-                if(i > j){
-                    swap(i, j);
-                }
-                for(int k = i; k < j; k++){
-                    if(abs(nums[k] - nums[k+1]) > maxDiff){
-                        edge = 0;
-                        break;
-                    }
-                }
-                if(edge){
+                if(abs(j-i) == abs(preEdge[j] - preEdge[i])){
                     ans[a] = 1;
                 }
+                else{
+                    ans[a] = 0;
+                }
+                // bool edge = 1;
+                // for(int k = i; k < j; k++){
+                //     if(abs(nums[k] - nums[k+1]) > maxDiff){
+                //         edge = 0;
+                //         break;
+                //     }
+                // }
+                // if(edge){
+                //     ans[a] = 1;
+                // }
             }
 
         }
