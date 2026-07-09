31586e3cf478436dd5399d6e3a1c616fab026427
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-09 11:48:51 +0530

    Time: 6 ms (85.48%), Space: 224 MB (78.36%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<bool> ans(queries.size(), 0);
-        vector<bool> adjedge(n, 1);
-        vector<int> preEdge(n, 1);
-        for(int i = 0; i < n-1; i++){
-            if(nums[i+1] - nums[i] > maxDiff){
-                adjedge[i+1] = 0;
-            }
-            preEdge[i+1] = preEdge[i] + adjedge[i+1];
-        }
-        for(int a = 0; a < queries.size(); a++){
-            int i = queries[a][0];
-            int j = queries[a][1];
-            if(abs(nums[i] - nums[j]) <= maxDiff){
-                ans[a] = 1;
-            }
-            else{
-                if(abs(j-i) == abs(preEdge[j] - preEdge[i])){
-                    ans[a] = 1;
-                }
-                else{
-                    ans[a] = 0;
-                }
-                // bool edge = 1;
-                // for(int k = i; k < j; k++){
-                //     if(abs(nums[k] - nums[k+1]) > maxDiff){
-                //         edge = 0;
-                //         break;
-                //     }
-                // }
-                // if(edge){
-                //     ans[a] = 1;
-                // }
-            }
+        vector<int> component(n, 0);
 
+        for (int i = 1; i < n; i++) {
+            component[i] = component[i - 1];
+
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                component[i]++;
+            }
         }
+
+        vector<bool> ans;
+        ans.reserve(queries.size());
+
+        for (auto& query : queries) {
+            int u = query[0];
+            int v = query[1];
+
+            ans.push_back(component[u] == component[v]);
+        }
+
         return ans;
     }
 };
