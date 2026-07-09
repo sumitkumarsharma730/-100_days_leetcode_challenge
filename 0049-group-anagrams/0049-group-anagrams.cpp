af53c16fbd1286eef81293e918bad906ba013a93
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-09 12:03:47 +0530

    Time: 12 ms (84.1%), Space: 25 MB (80.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,23 @@
+//Time Complexity: O(n × k log k), where k is average string length.
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        int n = strs.size();
-        if(n == 0) return  ans;
-        vector<vector<int>> v(n, vector<int>(26, 0));
-        for(int i = 0; i < n; i++){
-            int m = strs[i].size();
-            for(int j = 0; j < m; j++){
-                int val = strs[i][j] - 'a';
-                v[i][val]++;
-            }
+        unordered_map<string, vector<string>> mp;
+
+        for (string& str : strs) {
+            string key = str;
+
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(str);
         }
-        vector<int> marked(n, 0);
-        for(int i = 0; i < n; i++){
-            if(marked[i] == 1) continue;
-            marked[i] == 1;
-            vector<string> temp;
-            temp.push_back(strs[i]);
-            for(int j = i+1; j < n; j++){
-                bool match = 1;
-                for(int k = 0; k < 26; k++){
-                    if(v[i][k] != v[j][k]){
-                        match = 0;
-                        break;
-                    }
-                }
-                if(match == 1){
-                    temp.push_back(strs[j]);
-                    marked[j] = 1;
-                }
-            }
-            ans.push_back(temp);
+
+        vector<vector<string>> result;
+
+        for (auto& [key, group] : mp) {
+            result.push_back(group);
         }
-        return ans;
+
+        return result;
     }
 };
