e081e3d975972e9b18547524ede4515f43c0cae0
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-09 12:21:17 +0530

    Time: 22 ms (32.46%), Space: 26.4 MB (26.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,35 +2,22 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> mp;
-        vector<vector<string>> ans;
+        map<array<int, 26>, vector<string>> groups;
 
-        for (int i = 0; i < strs.size(); i++) {
+        for (string& word : strs) {
+            array<int, 26> freq = {};
 
-            int freq[26] = {0};
-
-            // Count frequency of each character
-            for (int j = 0; j < strs[i].size(); j++) {
-                char ch = strs[i][j];
+            for (char ch : word) {
                 freq[ch - 'a']++;
             }
 
-            // Create sorted word using frequency array
-            string newWord = "";
-
-            for (int j = 0; j < 26; j++) {
-                while (freq[j] > 0) {
-                    newWord += char('a' + j);
-                    freq[j]--;
-                }
-            }
-
-            mp[newWord].push_back(strs[i]);
+            groups[freq].push_back(word);
         }
 
-        // Store groups in answer
-        for (auto it : mp) {
-            ans.push_back(it.second);
+        vector<vector<string>> ans;
+
+        for (auto& group : groups) {
+            ans.push_back(group.second);
         }
 
         return ans;
