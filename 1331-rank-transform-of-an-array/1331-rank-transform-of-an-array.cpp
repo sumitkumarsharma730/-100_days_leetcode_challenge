128aa99a6f81e5a9bbe1282eca88c8609d40a3b9
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-12 13:10:39 +0530

    Time: 16 ms (98.94%), Space: 37.6 MB (97.48%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
+
         int n = arr.size();
-        vector<pair<int, int>> temp;
-        for(int i = 0; i < n; i++){
-            temp.push_back({arr[i], i});
-        }
+        if(n == 0) return {};
+
+        vector<pair<int,int>> temp;
+        temp.reserve(n);
+
+        for(int i = 0; i < n; i++)
+            temp.emplace_back(arr[i], i);
+
         sort(temp.begin(), temp.end());
-        vector<int> ans(n, 1);
+
+        vector<int> rank(n);
+
+        int currRank = 1;
+        rank[temp[0].second] = currRank;
+
         for(int i = 1; i < n; i++){
-            if(temp[i].first == temp[i-1].first){
-                ans[i] = ans[i-1];
-            }
-            else{
-                ans[i] = ans[i-1] + 1;
-            }
+
+            if(temp[i].first != temp[i-1].first)
+                currRank++;
+
+            rank[temp[i].second] = currRank;
         }
-        vector<int> rank(n, 0);
-        for(int i = 0; i < n; i++){
-            int j = temp[i].second;
-            rank[j] = ans[i];
-        }
+
         return rank;
-        
     }
 };
