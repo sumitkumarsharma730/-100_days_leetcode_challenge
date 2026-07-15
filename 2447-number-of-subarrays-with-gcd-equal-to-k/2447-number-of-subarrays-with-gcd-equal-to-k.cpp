4c5eed0a66c9b7855396b4b32238ac2bed1f5281
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-15 14:34:50 +0530

    Time: 0 ms (100%), Space: 13.6 MB (5.09%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -1,22 +1,37 @@
-// Time  : O(n² logM)
-// Space : O(1)
+// Time  : O(n log²M)   (practically near O(n logM))
+// Space : O(logM)
 // where M is the maximum array value
 class Solution {
 public:
     int subarrayGCD(vector<int>& nums, int k) {
+
+        vector<pair<int,int>> prev;
+
         int ans = 0;
-        for(int i = 0; i < nums.size(); i++){
-            int GCD = 0;
-            for(int j = i; j < nums.size(); j++){
-                GCD = gcd(GCD, nums[j]);
-                if(GCD < k){
-                    break;
-                }
-                if(GCD == k){
-                    ans++;
-                }
+
+        for(int x : nums){
+
+            vector<pair<int,int>> curr;
+
+            curr.push_back({x,1});
+
+            for(auto &[g,cnt] : prev){
+
+                int ng = gcd(g,x);
+
+                if(curr.back().first == ng)
+                    curr.back().second += cnt;
+                else
+                    curr.push_back({ng,cnt});
             }
+
+            for(auto &[g,cnt] : curr)
+                if(g == k)
+                    ans += cnt;
+
+            prev = move(curr);
         }
+
         return ans;
     }
 };
