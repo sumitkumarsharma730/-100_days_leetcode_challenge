ee3acb0d69408d3dad0fe4e76501ed105aad1af3
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-16 12:06:32 +0530

    Time: 2 ms (1.4%), Space: 7.9 MB (50.48%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        int ans = 0;
-        int GCD = gcd(a, b);
-        for(int i = 1; i*i <= GCD; i++){
-            if(GCD % i == 0){
-                if(i != GCD/i){
-                    ans += 2;
-                }
-                else{
-                    ans += 1;
-                }
+        int g = gcd(a, b);
+        int cnt = 0;
+
+        for (int i = 1; i * i <= g; i++) {
+            if (g % i == 0) {
+                cnt++;
+                if (i != g / i) cnt++;
             }
         }
-        return ans;
+
+        return cnt;
     }
 };
