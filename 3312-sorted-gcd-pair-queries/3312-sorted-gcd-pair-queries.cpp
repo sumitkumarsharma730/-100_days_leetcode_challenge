797b565efeaa2a5a6c49635c6c5a93c4978fe1a6
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-17 14:01:21 +0530

    Time: 47 ms (53.33%), Space: 129.9 MB (48%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -1,22 +1,25 @@
+// n = nums.size()
+// M = max(nums)
+// Q = queries.size()
 class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
-        int maxVal = *max_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());  // O(n)
 
-        vector<int> freq(maxVal + 1, 0);
+        vector<int> freq(maxVal + 1, 0);  // O(n)
         for (int x : nums)
             ++freq[x];
 
         // divCnt[g] = numbers divisible by g
-        vector<int> divCnt(maxVal + 1, 0);
-        for (int g = 1; g <= maxVal; ++g) {
+        vector<int> divCnt(maxVal + 1, 0);   
+        for (int g = 1; g <= maxVal; ++g) {  // O(MlogM)​
             for (int multiple = g; multiple <= maxVal; multiple += g) {
                 divCnt[g] += freq[multiple];
             }
         }
 
         // exact[g] = pairs having gcd exactly g
-        vector<long long> exact(maxVal + 1, 0);
+        vector<long long> exact(maxVal + 1, 0); // O(MlogM)​
         for (int g = maxVal; g >= 1; --g) {
 
             long long cnt = divCnt[g];
@@ -28,15 +31,15 @@ public:
         }
 
         // prefix[g] = pairs having gcd <= g
-        vector<long long> prefix(maxVal + 1, 0);
-        for (int g = 1; g <= maxVal; ++g) {
+        vector<long long> prefix(maxVal + 1, 0);  
+        for (int g = 1; g <= maxVal; ++g) {        // O(M)
             prefix[g] = prefix[g - 1] + exact[g];
         }
 
         vector<int> ans;
         ans.reserve(queries.size());
 
-        for (long long q : queries) {
+        for (long long q : queries) {   // O(QlogM)​
             ans.push_back(
                 lower_bound(prefix.begin() + 1, prefix.end(), q + 1) - prefix.begin()
             );
@@ -45,3 +48,5 @@ public:
         return ans;
     }
 };
+// Total time Complexity  -->  O(n + MlogM + QlogM)
+// Space Complexity: O(M) (for freq, divCnt, exact, and prefix).
