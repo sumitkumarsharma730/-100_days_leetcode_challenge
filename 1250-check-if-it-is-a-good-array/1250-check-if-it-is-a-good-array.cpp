b33016cea6a01cb05272e4063d0ba92f042bd2cd
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-17 16:52:18 +0530

    Time: 4 ms (35.86%), Space: 33.1 MB (58.77%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -1,13 +1,20 @@
+// For integers a₁, a₂, ..., aₙ, there exist integers x₁, x₂, ..., xₙ such that:
+
+// a₁x₁ + a₂x₂ + ... + aₙxₙ = gcd(a₁, a₂, ..., aₙ)
+
+// This is the exact form of Bézouts Identity
+
 class Solution {
 public:
     bool isGoodArray(vector<int>& nums) {
-        int GCD = 0;
-        for(int i = 0; i < nums.size(); i++){
-            GCD = gcd(GCD, nums[i]);
-            if(GCD == 1){
+        int g = 0;
+
+        for (int x : nums) {  // O(n)
+            g = gcd(g, x);    // O(logM)
+            if (g == 1)
                 return true;
-            }
         }
+
         return false;
     }
 };
