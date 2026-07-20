196e83455e2adac37098cc74cd0b226d086e2113
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-20 20:39:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
-    void insert(stack<char>& st, char c, bool& ans){
-        if(c == '('){
-            st.push(')');
-        }
-        else if(c == '['){
-            st.push(']');
-        }
-        else if(c == '{'){
-            st.push('}');
-        }
-        else {
-            ans = false;
-        }
-    }
     bool isValid(string s) {
-        int n = s.size();
         stack<char> st;
-        int i = 0;
-        while(i < n ){
-            if(st.empty()){
-                bool ans = true;
-                insert(st, s[i], ans);
-                if(ans == false){
-                    return ans;
-                }
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+                char t = st.top(); st.pop();
+                if ((c == ')' && t != '(') ||
+                    (c == ']' && t != '[') ||
+                    (c == '}' && t != '{'))
+                    return false;
             }
-            else{
-                if(s[i] == st.top()){
-                    st.pop();
-                }
-                else{
-                    bool ans = true;
-                    insert(st, s[i], ans);
-                    if(ans == false){
-                        return ans;
-                    }
-                }
-            }
-            i++;
         }
-        return (st.empty()) ?  true : false;
+        return st.empty();
     }
 };
