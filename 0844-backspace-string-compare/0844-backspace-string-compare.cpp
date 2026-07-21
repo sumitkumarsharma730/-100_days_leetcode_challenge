a18f1d556fcbeeebf12b695ef3f8c96d9cf6adc0
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-21 17:54:53 +0530

    Time: 2 ms (10.36%), Space: 8.4 MB (68.44%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,49 +1,31 @@
-//T.C -> O(n² + m²)
-// S.C -> (n + m)
+//T.C -> O(n + m)
+// S.C -> O(n + m)
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n = s.size();
-        string str = "";
-        for(int i = 0; i < n; i++){
-            if(s[i] == '#'){
-                if(!str.empty())
-                    str.pop_back();
+
+        string a, b;
+
+        for(char c : s){
+            if(c == '#'){
+                if(!a.empty())
+                    a.pop_back();
             }
             else{
-                str = str + s[i];   // Each concatenation copies all previous characters.  Cost Analysis  ---> O(n²)
+                a.push_back(c);
             }
         }
-        int m = t.size();
-        string str2 = "";
-        for(int i = 0; i < m; i++){
-            if(t[i] == '#'){
-                if(!str2.empty())
-                    str2.pop_back();
+
+        for(char c : t){
+            if(c == '#'){
+                if(!b.empty())
+                    b.pop_back();
             }
             else{
-                str2 = str2 + t[i];
+                b.push_back(c);
             }
         }
-        return str == str2;
+
+        return a == b;
     }
 };
-
-
-/*
-Small Optimization
-
-Instead of
-
-str = str + s[i];
-
-use
-
-str.push_back(s[i]);
-
-Similarly,
-
-str2.push_back(t[i]);
-
-Now each insertion is amortized O(1).
-*/
