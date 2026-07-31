cb83660e1d5ecfd6a4c874b5f8a8b22b5bf77023
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-07-31 19:02:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (31.56%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n = word.size();
-        int layers = 1;
         int ans = 0;
-        while(n != 0){
-            if(n > 8){
-                n -= 8;
-                ans += 8*layers++;
-            }
-            else{
-                ans += n*layers;
-                n = 0;
-            }
+
+        for (int i = 0; i < word.size(); i++) {
+            ans += (i / 8) + 1;
         }
+
         return ans;
     }
 };
