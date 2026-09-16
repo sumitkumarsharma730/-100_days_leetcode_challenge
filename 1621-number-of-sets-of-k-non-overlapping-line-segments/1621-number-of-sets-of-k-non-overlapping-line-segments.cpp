1757ca681c2518d5b78e455ba714943826fc0bfd
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-09-16 23:41:53 +0530

    Time: 32 ms (65.42%), Space: 31.1 MB (45.33%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     static const int MOD = 1e9 + 7;
-    vector<vector<int>> dp;
-    // int cnt = 0;
-    int remainSet(int &n, int k, int s){
-        if(k == 0){
-            return 1;
-        }
 
-        if(n - s - 1 < k)
-            return 0;
-
-        long long cnt = 0;
-
-        if(dp[s][k] != -1){
-            return dp[s][k];
-        }
-
-        cnt = (cnt + remainSet(n, k, s+1)) % MOD;
-
-        for(int i = s + 1; i < n; i++){
-            cnt = (cnt + remainSet(n, k-1, i)) % MOD;
-        }
-        return dp[s][k] = cnt;
-    }
     int numberOfSets(int n, int k) {
-        dp.assign(n, vector<int>(k+1, -1));
-        // int ans = 0;
-        // for(int i = 1; i < n; i++){
-        //     dp[i][k-1] = remainSet(n, k-1, i);
-        //     ans
-        // }
-        return remainSet(n, k, 0);
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1));
+
+        for(int i = 0; i < n; i++)
+            dp[0][i] = 1;
+
+        for(int ki = 1; ki <= k; ki++) {
+            long long suffix = 0;
+
+            for(int i = n - 1; i >= 0; i--) {
+                suffix = (suffix + dp[ki - 1][i + 1]) % MOD;
+                dp[ki][i] = (dp[ki][i + 1] + suffix) % MOD;
+            }
+        }
+
+        return dp[k][0];
     }
 };
