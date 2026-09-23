e98482a9ae613492c7ceeb75d51b2b6f59435ce5
sumitkumarsharma730 <sumitkumarsharma1646@gmail.com>
2026-09-23 13:32:00 +0530

    Time: 0 ms (100%), Space: 8.7 MB (99.78%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,24 +1,32 @@
+// Formula used:
+
+// C(n, i) = C(n, i-1) × (n-i+1) / i
+
+// For n = 5:
+
+// C(5,0) = 1
+
+// C(5,1) = 1 × 5 / 1 = 5
+
+// C(5,2) = 5 × 4 / 2 = 10
+
+// C(5,3) = 10 × 3 / 3 = 10
+
+// C(5,4) = 10 × 2 / 4 = 5
+
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        ans.push_back(1);
-        if(rowIndex == 0){
-            return ans;
+
+        vector<int> ans(rowIndex + 1, 1);
+
+        long long value = 1;
+
+        for(int i = 1; i < rowIndex; i++) {
+            value = value * (rowIndex - i + 1) / i;
+            ans[i] = value;
         }
-        int i = 1;
-        while(i <= rowIndex){
-            vector<int> temp;
-            temp.push_back(1);
-            int j = 1;
-            while(j < i){
-                temp.push_back(ans[j] + ans[j-1]);
-                j++;
-            }
-            temp.push_back(1);
-            i++;
-            ans = temp;
-        }
+
         return ans;
     }
 };
